fdc85ed312e440e77b8711064d3d46294504db9a
gunruethaiz <kanruethai.741236@gmail.com>
2024-11-19 12:03:56 +0700

    Update

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Kanruethai"
+last_name="Kaewsawang"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="Kanruethai"
+last_name="Kaewsawang"
+whole_name = last_name + first_name
+print(whole_name)
